695a1f2371bf296fd3c2cec1b730280f3af2dcf9
Abdallah Anwar Mohamed <91182894+AnwarTech1@users.noreply.github.com>
2022-07-24 10:28:22 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Make The Background Of Navbar Change
+let myNavbar = document.querySelector(".ftco-navbar-light");
+this.window.onscroll = function () {
+  if ((window.scrollY = 10)) {
+    myNavbar.style.backgroundColor = "#fff";
+  }
+};
